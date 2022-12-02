3fe5f23fe978ac7fc0c5b2c066d259f520a7320c
Vladimir Tarasenko <107504813+Vlatarasenko@users.noreply.github.com>
2022-12-02 18:15:01 +0200

    Add files via upload

---
+++ src/js/01-gallery.js
@@ -0,0 +1,37 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+const markup = galleryItems.map(({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</div>`).join('');
+
+const galleryList = document.querySelector('.gallery');
+
+galleryList.insertAdjacentHTML("afterbegin", markup);
+
+galleryList.addEventListener('click', onGalleryContainerClick);
+
+function onGalleryContainerClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return
+    }
+
+    // ----SimpleLightbox----
+
+};
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  showCounter: false,
+});
+
+gallery.on("show.simplelightbox");
+
+
